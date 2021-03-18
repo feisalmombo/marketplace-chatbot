45dc432b8cb682c73d39f94e28d805bc741c8deb
Feisal Mombo <feisalmombo29@gmail.com>
2021-03-18 14:23:51 +0300

    fix bugs

--- app/Http/Controllers/BotManController.php
+++ app/Http/Controllers/BotManController.php
@@ -19,7 +19,7 @@ class BotManController extends Controller
             if ($message == 'compare') {
                 $this->startConversation($botman);
             }else{
-                $botman->reply("write 'compare' to start...");
+                $botman->reply("Wrong entry keyword please. Write 'compare' keyword to start...");
             }
 
         });
