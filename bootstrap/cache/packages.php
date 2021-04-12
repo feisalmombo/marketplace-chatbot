8d00771da5bd7b248c26b6ad8afabee577b27cce
Feisal Mombo <feisalmombo29@gmail.com>
2021-04-12 13:26:31 +0300

    fix bootstrap/cache

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,119 @@
+<?php return array (
+  'beyondcode/laravel-dump-server' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'BeyondCode\\DumpServer\\DumpServerServiceProvider',
+    ),
+  ),
+  'botman/botman' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'BotMan\\BotMan\\BotManServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'BotMan' => 'BotMan\\BotMan\\Facades\\BotMan',
+    ),
+  ),
+  'botman/driver-botframework' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'BotMan\\Drivers\\BotFramework\\Providers\\BotFrameworkServiceProvider',
+    ),
+  ),
+  'botman/driver-facebook' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'BotMan\\Drivers\\Facebook\\Providers\\FacebookServiceProvider',
+    ),
+  ),
+  'botman/driver-hipchat' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'BotMan\\Drivers\\HipChat\\Providers\\HipChatServiceProvider',
+    ),
+  ),
+  'botman/driver-nexmo' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'BotMan\\Drivers\\Nexmo\\Providers\\NexmoServiceProvider',
+    ),
+  ),
+  'botman/driver-slack' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'BotMan\\Drivers\\Slack\\Providers\\SlackServiceProvider',
+    ),
+  ),
+  'botman/driver-telegram' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'BotMan\\Drivers\\Telegram\\Providers\\TelegramServiceProvider',
+    ),
+  ),
+  'botman/driver-twilio' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'BotMan\\Drivers\\Twilio\\Providers\\TwilioServiceProvider',
+    ),
+  ),
+  'botman/driver-web' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'BotMan\\Drivers\\Web\\Providers\\WebServiceProvider',
+    ),
+  ),
+  'botman/driver-wechat' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'BotMan\\Drivers\\WeChat\\Providers\\WeChatServiceProvider',
+    ),
+  ),
+  'botman/studio-addons' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'BotMan\\Studio\\Providers\\StudioServiceProvider',
+      1 => 'BotMan\\Studio\\Providers\\RouteServiceProvider',
+    ),
+  ),
+  'botman/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'BotMan\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'fideloper/proxy' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+);
