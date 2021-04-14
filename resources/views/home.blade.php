f1ca129191c7582ffde152413dfb349575e8ba1c
Feisal Mombo <feisalmombo29@gmail.com>
2021-04-14 18:34:00 +0300

    modify home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,7 +24,7 @@
             <span class="info-box-icon bg-light-blue"><i class="fa fa-users"></i></span>
             <div class="info-box-content">
               <span class="info-box-text">Users</span>
-              <span class="info-box-number">{{ $usersCount[0]->usersCount }}</span>
+              <span class="info-box-number">12</span>
             </div><!-- /.info-box-content -->
           </div><!-- /.info-box -->
           </a>
@@ -39,7 +39,7 @@
             <span class="info-box-icon bg-light-blue"><i class="fa fa-square"></i></span>
             <div class="info-box-content">
               <span class="info-box-text">Posts</span>
-              <span class="info-box-number">{{ $postsCount[0]->postsCount }}</span>
+              <span class="info-box-number">12</span>
             </div><!-- /.info-box-content -->
           </div><!-- /.info-box -->
          </a>
@@ -52,7 +52,7 @@
             <span class="info-box-icon bg-blue"><i class="fa fa-indent"></i></span>
             <div class="info-box-content">
               <span class="info-box-text">Categories</span>
-              <span class="info-box-number">{{ $categoriesCount[0]->categoriesCount }}</span>
+              <span class="info-box-number">12</span>
             </div><!-- /.info-box-content -->
           </div><!-- /.info-box -->
            </a>
@@ -68,7 +68,7 @@
          <span class="info-box-icon bg-light-blue"><i class="fa fa-user-plus"></i></span>
          <div class="info-box-content">
            <span class="info-box-text">Subscribers</span>
-           <span class="info-box-number">{{ $subscribersCount[0]->subscribersCount }}</span>
+           <span class="info-box-number">12</span>
          </div><!-- /.info-box-content -->
        </div><!-- /.info-box -->
       </a>
@@ -81,25 +81,12 @@
          <span class="info-box-icon bg-blue"><i class="fa fa-comment"></i></span>
          <div class="info-box-content">
            <span class="info-box-text">Comments</span>
-           <span class="info-box-number">{{ $commentsCount[0]->commentsCount }}</span>
+           <span class="info-box-number">12</span>
          </div><!-- /.info-box-content -->
        </div><!-- /.info-box -->
         </a>
      </div>
 
-
-    {{--<div class="col-lg-4">
-    <a href="{{ url('/request-customer') }}">
-            <div class="info-box">
-            <!-- Apply any bg-* class to to the icon to color it -->
-            <span class="info-box-icon bg-blue"><i class="fa fa-registered"></i></span>
-            <div class="info-box-content">
-              <span class="info-box-text">Request</span>
-              <span class="info-box-number">9</span>
-            </div><!-- /.info-box-content -->
-          </div><!-- /.info-box -->
-           </a>
-    </div> --}}
     @endif
 
 </section>
